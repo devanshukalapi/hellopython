98f863a8c15e8103dffac02639226d30c02321da
Ajay Kotian <kotianajay304@gmail.com>
2016-10-08 11:21:14 +0530

    added odd even

---
+++ oddeven.py
@@ -0,0 +1,9 @@
+def isEven(number):
+      if number%2 ==0:
+         print("this is even number")
+      else:
+          print("this is an odd number")
+
+num=int(input("enter a numbers"))
+
+isEven(num)
